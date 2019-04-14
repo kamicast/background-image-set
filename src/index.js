95931d7f802792149b71898683d10463d86e2adf
CraigglesO <oconnorct1@gmail.com>
2019-04-13 20:18:01 -0400

    yay

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import fs      from 'fs';
+import isImage from 'is-image';
+
+const githubDirectory = 'https://github.com/CraigglesO/background-image-set/raw/master/images/';
+
+(function() {
+  // find all images in "images" directory, save each one to a JSON file:
+  let fileList = fs.readdirSync('./images');
+  fileList.filter(isImage); // filter out any files that are not images...
+  fileList = fileList.map(image => {
+    return githubDirectory + image;
+  });
+  fs.writeFileSync('images.json', JSON.stringify(fileList));
+})();
